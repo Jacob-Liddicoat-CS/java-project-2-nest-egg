be7a863de93c92fb8092e826c61c5ba2a5b48c4c
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-04-14 15:49:11 +0000

    Add comments explaining parts of the code

--- nestEgg.java
+++ nestEgg.java
@@ -1,3 +1,4 @@
+//Calculates your retirement fund each year
 import java.io.*;
 import java.util.*;
 public class nestEgg
@@ -6,12 +7,12 @@ public class nestEgg
  {
   double salary;
   System.out.println("Hello. What is your salary? ");
-  salary = keyboard.nextDouble();
+  salary = keyboard.nextDouble();//Inputs your salary
   double save = 5;
   double growthRate = 15;
-  double F1 = salary * save * 0.01;
-  double F2 = F1 * (1+0.01 * growthRate) + salary * save * 0.01;
-  double F3 = F2 * (1+0.01 * growthrate) + salary * save * 0.01;
+  double F1 = salary * save * 0.01;//Calculates your retirement fund for the first year
+  double F2 = F1 * (1+0.01 * growthRate) + salary * save * 0.01;//Calculates your retirement fund for the second year
+  double F3 = F2 * (1+0.01 * growthrate) + salary * save * 0.01;//Calculates your retirement fund for the third year
 
   System.out.println("End of year 1:" + F1);
   System.out.println("End of year 2:" + F2);
