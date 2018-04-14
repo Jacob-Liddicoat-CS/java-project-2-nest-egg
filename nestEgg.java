91faeb2e9dc9d4cd796e92cf5d4ce28688d278da
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-04-14 15:59:41 +0000

    Fix typo and error with scanner

--- nestEgg.java
+++ nestEgg.java
@@ -6,13 +6,14 @@ public class nestEgg
  public static void main(String args[])
  {
   double salary;
+  Scanner input = new Scanner(System.in);//Generates Scanner that scans the numbers you input
   System.out.println("Hello. What is your salary? ");
   salary = keyboard.nextDouble();//Inputs your salary
   double save = 5;
   double growthRate = 15;
   double F1 = salary * save * 0.01;//Calculates your retirement fund for the first year
   double F2 = F1 * (1+0.01 * growthRate) + salary * save * 0.01;//Calculates your retirement fund for the second year
-  double F3 = F2 * (1+0.01 * growthrate) + salary * save * 0.01;//Calculates your retirement fund for the third year
+  double F3 = F2 * (1+0.01 * growthRate) + salary * save * 0.01;//Calculates your retirement fund for the third year
 
   System.out.println("End of year 1:" + F1);
   System.out.println("End of year 2:" + F2);
